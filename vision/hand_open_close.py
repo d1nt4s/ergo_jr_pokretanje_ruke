c68b45272a29d508447348f7a9e7bcf578b6ffc5
raf <vanzemunac620@>
2025-12-24 15:07:13 +0000

    changed main to recognize light and dark from camera

--- vision/hand_open_close.py
+++ vision/hand_open_close.py
@@ -3,31 +3,23 @@ import cv2
 
 class HandOpenCloseDetector:
     def __init__(self):
-        self.open_threshold = 9000
-        self.close_threshold = 4000
+        # Подбирается очень просто (см. ниже)
+        self.open_threshold = 80   # светло
+        self.close_threshold = 40  # темно
 
     def infer(self, frame_bgr):
+        # Переводим в серый
         gray = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2GRAY)
-        blur = cv2.GaussianBlur(gray, (7, 7), 0)
 
-        _, thresh = cv2.threshold(
-            blur, 0, 255,
-            cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU
-        )
+        # Средняя яркость кадра
+        mean_val = gray.mean()
 
-        contours, _ = cv2.findContours(
-            thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
-        )
+        # Для отладки можно печатать
+        # print("MEAN:", mean_val)
 
-        if not contours:
-            return "UNKNOWN"
-
-        hand_contour = max(contours, key=cv2.contourArea)
-        area = cv2.contourArea(hand_contour)
-
-        if area > self.open_threshold:
+        if mean_val > self.open_threshold:
             return "OPEN"
-        elif area < self.close_threshold:
+        elif mean_val < self.close_threshold:
             return "CLOSE"
         else:
             return "UNKNOWN"
